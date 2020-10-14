c643d0525eccb719653e331b6d4591fa179c4ee8
abdulhamidnugroho <abdulhamidnugroho@gmail.com>
2020-10-14 14:42:13 +0700

    fix for undefined props

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
-
+import { Contex, Context } from '../context/BlogContext';
 import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
-    const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
+    const { state } = useContext(Context);
+    const blogPost = state.find(
+        blogPost => blogPost.id === navigation.getParam('id')
+    );
     
-    return <BlogPostForm />;
+    return (
+        <BlogPostForm 
+            initialValues={{ title: blogPost.title, content: blogPost.content }}
+            onSubmit={(title, content) => {
+                console.log(title, content)
+            }}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
